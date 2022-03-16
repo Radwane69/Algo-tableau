874595d1f9a2faf82c60a9404bdaac25871dba69
Radwane69 <rnougha@edenschool.fr>
2022-03-16 11:32:27 +0100

    import export

---
+++ js/app.js
@@ -0,0 +1,11 @@
+export const minimum = (entry) => {
+
+    return entry;
+}
+
+function minimum(minimum) {
+    var resultat = [];
+    for (var i = 0; i != minimum.length; i++)
+        resultat[i] = (minimum[i]);
+    return resultat;
+}
