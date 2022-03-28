bfeecf960587806c88342d0af79bffc3410cbbc5
Radwane69 <rnougha@edenschool.fr>
2022-03-28 16:37:09 +0200

    commit content

--- js/app.js
+++ js/app.js
@@ -1,7 +1,27 @@
-export const minimum = (Array) => {
-    var resultat = [];
-    for (var i = 0; i != minimum.length; i++)
-        resultat[i] = (minimum[i]);
-    return resultat;
+export const minimum = (arr) => {
+    let mini = arr[0];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < mini) {
+            mini = arr[i];
+        }
+    }
+    return mini;
 }
 
+export const moyenne = (Array) => {
+    let moyenn = 0;
+    for (let i = 0; i < Array.length; i++) {
+        moyenn + Array[i];
+    }
+    return moyenn / Array.length;
+}
+
+export function sanslesstring(arra) {
+    let arra = [];
+    for (let i = 0; i < arra.length; i++) {
+        if (typeof arra[i] != 'string') {
+            arra.push(arra[i]);
+        }
+    }
+    return arra;
+}
