c0ea17c8a157d262ebad3ec15f5bc69c1517d5c0
Radwane69 <rnougha@edenschool.fr>
2022-04-05 14:28:45 +0200

    commit content

--- js/app.js
+++ js/app.js
@@ -1,27 +1,39 @@
 export const minimum = (arr) => {
-    let mini = arr[0];
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] < mini) {
-            mini = arr[i];
-        }
+  let mini = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < mini) {
+      mini = arr[i];
     }
-    return mini;
-}
+  }
+  return mini;
+};
 
 export const moyenne = (Array) => {
-    let moyenn = 0;
-    for (let i = 0; i < Array.length; i++) {
-        moyenn + Array[i];
+  let moyenn = 0;
+  for (let i = 0; i < Array.length; i++) {
+    moyenn + Array[i];
+  }
+  return moyenn / Array.length;
+};
+
+export function sanslesstring(arr) {
+  let arr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] != "string") {
+      arr.push(arr[i]);
     }
-    return moyenn / Array.length;
+  }
+  return arr;
 }
 
-export function sanslesstring(arra) {
-    let arra = [];
-    for (let i = 0; i < arra.length; i++) {
-        if (typeof arra[i] != 'string') {
-            arra.push(arra[i]);
-        }
+export const impairetPair = (arr) => {
+  let array = [[], []];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 == 1 || arr[i] % 2 == -1) {
+      array[0].push(arr[i]);
+    } else {
+      array[1].push(arr[i]);
     }
-    return arra;
+  }
+  return array;
 }
